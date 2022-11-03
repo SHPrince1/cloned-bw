c59eb83614ecfa39147f556f0cf6844433407123
Sylvanus Prince <princehsylvanus@gmail.com>
2022-11-03 07:43:19 +0100

    ddd

--- src/components/topNav.js
+++ src/components/topNav.js
@@ -1,5 +1,5 @@
 import React from "react";
-import toStyle from "../styles/topnav.module.css";
+import Style from "../styles/topnav.module.css";
 import mainLogo from "../images/bwlogo.png";
 
 import {AiOutlineMail } from "react-icons/ai";
@@ -8,11 +8,11 @@ import {MdCall} from "react-icons/md";
 const TopNav = () => {
   return (
     <>
-      <div className={toStyle.topNavBox}>
-        <div className={toStyle.imgBox}>
+      <div className={Style.topNavBox}>
+        <div className={Style.imgBox}>
           <img src={mainLogo} alt="fireSpot" />
         </div>
-        <div className={toStyle.emailBox}>
+        <div className={Style.emailBox}>
          <AiOutlineMail  size={35}/>
           <p>info@bankerswarehouse.com</p>
           <MdCall size={35} />
